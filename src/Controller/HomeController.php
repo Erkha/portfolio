3309c0d49f4fc7f6c2a0585dabd689583cd2e56a
erkha <jmtatout@gmail.com>
2019-07-17 11:47:26 +0200

    added skills on main page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,16 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(UserRepository $userRepo)
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'user' => $userRepo->findOneBy(['username' => 'admin'])
         ]);
     }
 }
